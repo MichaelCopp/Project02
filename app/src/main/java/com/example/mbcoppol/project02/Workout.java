a3c26cd8a7e07c098bd8e3c4da9826a78b2703e7
mbcoppol <michaelbcoppolino@gmail.com>
2016-12-07 15:05:46 -0500

    Added More Workouts, and Fixed Errors

--- app/src/main/java/com/example/mbcoppol/project02/Workout.java
+++ app/src/main/java/com/example/mbcoppol/project02/Workout.java
@@ -14,12 +14,14 @@ public class Workout {
                     "10 Deep Breaths\n2 sets Arm Extensions\n2 set Leg Extension",
                     "10 minutes"),
             new Workout ("Core Agony",
-                    "100 pull-ups\n100 push-ups\n100 squats","15 Minutes"),
+                    "100 pull-ups\n100 push-ups\n100 squats","25 Minutes"),
             new Workout ("Yoga Routine",
                     "Downward Dog\nSun Salutation\nChair\nWarrior", "20 Minutes"),
-            new Workout ("Distance and Power",
+            new Workout ("High Impact",
                     "500 meter run\n21 x 1.5 pood kettleball swing\n21 x pull-ups", "30 minutes"),
-            new Workout ("Winter Special", "1000 Rising Steps\n100 Hula Hoop Swings", "30 Minutes")
+            new Workout ("Winter Special", "1000 Rising Steps\n100 Hula Hoop Swings", "30 Minutes"),
+            new Workout ("Standing Workout", "30 Lunges\n100 Jumping Jacks\nTouch Your Toes", "20 Minutes")
+
     };
 
     private Workout(String name, String description, String length) {
